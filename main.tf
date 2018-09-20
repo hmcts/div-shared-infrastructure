dd68141a9ea29c23c75827e9cd5fbce3f619b4fe
tony.chow <tony.chow@hmcts.net>
2018-09-20 01:19:03 +0100

    Update with Shared Infra settings

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.env}"
+resource "azurerm_resource_group" "shared_resource_group" {
+  name = "${var.product}-shared-infrastructure-${var.env}"
   location = "${var.location}"
+  tags {
+        "Deployment Environment" = "${var.env}"
+        "Team Name" = "${var.team_name}"
+        "Team Contact" = "${var.team_contact}"
+        "Destroy Me" = "${var.destroy_me}"
+  }
 }
