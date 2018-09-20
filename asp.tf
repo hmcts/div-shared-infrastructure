471e0b9587cd81e3d077c17ec2e648e892ca37b6
tony.chow <tony.chow@hmcts.net>
2018-09-20 01:34:20 +0100

    Fix invalid service plan

--- asp.tf
+++ asp.tf
@@ -5,11 +5,11 @@ locals {
 }
 module "appServicePlan" {
     source = "git@github.com:hmcts/cnp-module-app-service-plan?ref=master"
-    location = "UK South"
+    location = "${var.location}"
     env = "${var.env}"
     resource_group_name = "${azurerm_resource_group.rg.name}"
     asp_capacity = "${var.asp_capacity}"
-    asp_name = "${var.product}-${var.env}§"
+    asp_name = "${var.product}"
     ase_name = "${local.ase_name}"
     tag_list = "${local.common_tags}"
 }
