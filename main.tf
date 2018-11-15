833cbaf74ac1c5a551bdacf1f917289e18145ddd
Hosain <hosain.alahmad@hmcts.net>
2018-11-15 12:19:09 +0000

    Set resource group tags
    As instructed by https://tools.hmcts.net/confluence/display/CNP/Governance+and+Tagging

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
+variable "team_contact" {
+  default     = "#div-dev"
+  description = "Slack channel team can be reached on for support"
+}
+
+locals {
+  tags = "${merge(
+    var.common_tags,
+    map(
+      "Team Contact", var.team_contact
+    )
+  )}"
+}
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = "${var.location}"
+
+  tags = "${local.tags}"
 }
