76f2257a0173a71704a6bbeb42f107a83c073675
Ganesh Raja <ganeshraja.ramu@hmcts.net>
2019-01-11 00:46:34 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,7 @@ variable "team_contact" {
 locals {
   tags = "${merge(
     var.common_tags,
-    map("Deployment Environment", var.env),
-    map("Team Name", var.team_name),
-    map("Team Contact", var.team_contact),
-    map("Destroy Me", var.destroy_me)
+    map("Team Contact", var.team_contact)
   )}"
 }
 
