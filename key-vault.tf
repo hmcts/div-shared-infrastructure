efa36af873f4e55387da44e824ec3b5cfcb4d504
Hosain <hosain.alahmad@hmcts.net>
2019-04-03 15:26:32 +0100

    DIV-3956 - added missing common_tags for div-vault

--- key-vault.tf
+++ key-vault.tf
@@ -7,6 +7,7 @@ module "div-vault" {
   object_id               = "${var.jenkins_AAD_objectId}"
   resource_group_name     = "${azurerm_resource_group.rg.name}"
   product_group_object_id = "3450807e-5248-4053-944f-9df59dda50b9"
+  common_tags             = "${var.common_tags}"
 }
 
 output "vaultName" {
