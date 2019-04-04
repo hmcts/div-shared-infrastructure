b9480ea396dbac95933e9294ce673e2f4f5862b0
Hosain <hosain.alahmad@hmcts.net>
2019-04-04 10:19:43 +0100

    DIV-3956 - added performance alert
    Triggers when a FE page takes longer than 10 seconds

--- alerts.tf
+++ alerts.tf
@@ -35,3 +35,22 @@ module "div-server-errors-alert" {
   trigger_threshold = 5
   resourcegroup_name = "${azurerm_resource_group.rg.name}"
 }
+
+module "div-fe-performance-alert" {
+  source = "git@github.com:hmcts/cnp-module-metric-alert"
+  location = "${var.location}"
+
+  app_insights_name = "div-${var.env}"
+
+  alert_name = "div-fe-performance-alert"
+  alert_desc = "Web pages took longer than 10 seconds to load in div-${var.env}."
+  app_insights_query = "requests | where url !contains \"/health\" | where success == \"True\" | where duration > 10000 | where cloud_RoleName in (\"div-pfe\", \"div-rfe\", \"div-dn\")"
+  custom_email_subject = "Alert: performance errors in div-${var.env}"
+  frequency_in_minutes = 5
+  time_window_in_minutes = 5
+  severity_level = "2"
+  action_group_name = "Divroce Support"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold = 5
+  resourcegroup_name = "${azurerm_resource_group.rg.name}"
+}
