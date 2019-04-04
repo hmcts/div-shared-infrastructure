b9db869e29012af845b1f978734978eb6af9613e
Hosain <hosain.alahmad@hmcts.net>
2019-04-04 09:55:36 +0100

    DIV-3956 - added server errors alert

--- alerts.tf
+++ alerts.tf
@@ -4,10 +4,29 @@ module "div-bad-requests-alert" {
 
   app_insights_name = "div-${var.env}"
 
-  alert_name = "bad-requests"
+  alert_name = "div-bad-requests"
   alert_desc = "Found HTTP requests with 400 error response code (bad request) in div-${var.env}."
   app_insights_query = "requests | where resultCode == \"400\""
   custom_email_subject = "Alert: bad requests in div-${var.env}"
+  frequency_in_minutes = 5
+  time_window_in_minutes = 5
+  severity_level = "2"
+  action_group_name = "Divroce Support"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold = 5
+  resourcegroup_name = "${azurerm_resource_group.rg.name}"
+}
+
+module "div-server-errors-alert" {
+  source = "git@github.com:hmcts/cnp-module-metric-alert"
+  location = "${var.location}"
+
+  app_insights_name = "div-${var.env}"
+
+  alert_name = "div-server-errors"
+  alert_desc = "Found HTTP requests with 400 error response code (bad request) in div-${var.env}."
+  app_insights_query = "requests | where resultCode startswith \"5\"  | where name !contains \"/health\""
+  custom_email_subject = "Alert: server errors in div-${var.env}"
   frequency_in_minutes = 5
   time_window_in_minutes = 5
   severity_level = "2"
