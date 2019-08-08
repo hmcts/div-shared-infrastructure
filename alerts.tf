c9ec9e92a7492dfcdb1885bca7f57ebb5272a1c8
Alex Wakeman <wakemana@gmail.com>
2019-08-08 10:56:30 +0100

    Fix infra quotes

--- alerts.tf
+++ alerts.tf
@@ -6,7 +6,7 @@ module "div-bad-requests-alert" {
 
   alert_name = "div-bad-requests"
   alert_desc = "Found HTTP requests with 400 or 422 error response codes (bad request) in div-${var.env}."
-  app_insights_query = "requests | where resultCode in ('400', '422')"
+  app_insights_query = "requests | where resultCode in (\"400\", \"422\")"
   custom_email_subject = "Alert: bad requests in div-${var.env}"
   frequency_in_minutes = 5
   time_window_in_minutes = 5
@@ -26,7 +26,7 @@ module "div-server-errors-alert" {
 
   alert_name = "div-server-errors"
   alert_desc = "Found HTTP requests with 500 error response code (bad request) in div-${var.env}."
-  app_insights_query = "requests | where resultCode startswith '5'  | where name !contains '/health' | where datetime_diff('hour', timestamp, startofday(timestamp)) !between (2 .. 5) or url !contains '-aat'"
+  app_insights_query = "requests | where resultCode startswith \"5\"  | where name !contains \"/health\" | where datetime_diff(\"hour\", timestamp, startofday(timestamp)) !between (2 .. 5) or url !contains \"-aat\""
   custom_email_subject = "Alert: server errors in div-${var.env}"
   frequency_in_minutes = 5
   time_window_in_minutes = 5
@@ -45,7 +45,7 @@ module "div-fe-performance-alert" {
 
   alert_name = "div-fe-performance-alert"
   alert_desc = "Web pages took longer than 10 seconds to load in div-${var.env}."
-  app_insights_query = "requests | where url !contains '/health' | where success == 'True' | where duration > 10000 | where cloud_RoleName in ('div-pfe', 'div-rfe', 'div-dn')"
+  app_insights_query = "requests | where url !contains \"/health\" | where success == \"True\" | where duration > 10000 | where cloud_RoleName in (\"div-pfe\", \"div-rfe\", \"div-dn\")"
   custom_email_subject = "Alert: performance errors in div-${var.env}"
   frequency_in_minutes = 5
   time_window_in_minutes = 5
@@ -64,7 +64,7 @@ module "div-bulkcase-errors-alert" {
 
   alert_name = "div-bulkcase-errors"
   alert_desc = "Bulk case update failed on in div-${var.env}."
-  app_insights_query = "traces | where message has 'Bulk case update failed' |  where severityLevel == 3"
+  app_insights_query = "traces | where message has \"Bulk case update failed\" |  where severityLevel == 3"
   custom_email_subject = "Alert: Bulk case update errors in div-${var.env}"
   frequency_in_minutes = 5
   time_window_in_minutes = 5
