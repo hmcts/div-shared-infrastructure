8955b1abcdffd79395ede1ac650d35892e00aba6
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-02-17 16:32:32 +0000

    Use correct group for divorce key vault (#68)

--- key-vault.tf
+++ key-vault.tf
@@ -1,13 +1,12 @@
 module "div-vault" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=azurermv2"
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                    = "${var.product}-${var.env}"
   product                 = var.product
   env                     = var.env
   tenant_id               = var.tenant_id
   object_id               = var.jenkins_AAD_objectId
   resource_group_name     = azurerm_resource_group.rg.name
-
-  product_group_object_id = var.product_group_object_id
+  product_group_name      = "dcd_divorce"
   common_tags             = var.common_tags
   managed_identity_object_id = var.managed_identity_object_id
 }
