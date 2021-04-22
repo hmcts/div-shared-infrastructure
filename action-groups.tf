eeb40b20ffb1c45dd780cfcde7cc8a482b71d0cf
damongreen123 <damon.green@btinternet.com>
2021-04-22 11:49:57 +0100

    Update action-groups.tf

--- action-groups.tf
+++ action-groups.tf
@@ -1,6 +1,11 @@
 // Divorce Alerts Action Groups
 
-data "azurerm_key_vault_secret" "divorce_support_email_secret" {
+data "azurerm_key_vault_secret" "divorce_support_email2_secret" {
+  name      = "divorce-support-email2"
+  key_vault_id = module.div-vault.key_vault_id
+}
+  
+data "azurerm_key_vault_secret" "divorce_support_email2_secret" {
   name      = "divorce-support-email"
   key_vault_id = module.div-vault.key_vault_id
 }
@@ -15,4 +20,16 @@ module "divorce-action-group" {
   short_name             = "div-support"
   email_receiver_name    = "Divorce Support Mailing List"
   email_receiver_address = data.azurerm_key_vault_secret.divorce_support_email_secret.value
+}
+
+  module "divorce-action-group2" {
+  source   = "git@github.com:hmcts/cnp-module-action-group"
+  location = "global"
+  env      = var.env
+
+  resourcegroup_name     = azurerm_resource_group.rg.name
+  action_group_name      = "div-support2"
+  short_name             = "div-support2"
+  email_receiver_name    = "Divorce Support Mailing List2"
+  email_receiver_address = data.azurerm_key_vault_secret.divorce_support_email2_secret.value
 }
