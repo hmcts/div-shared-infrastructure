3f9ff430e72118b78ec777c272ad2cd1c718a001
Damon Green <damon.green@btinternet.com>
2023-02-22 11:26:43 +0000

    https://tools.hmcts.net/jira/browse/DTSSE-2873 Updating TF version.

--- application-insights.tf
+++ application-insights.tf
@@ -1,19 +1,9 @@
-locals {
-  ai_tags = "${merge(
-        var.common_tags,
-        map("Deployment Environment", var.env),
-        map("Team Name", var.team_name),
-        map("Team Contact", var.team_contact),
-        map("Destroy Me", var.destroy_me)
-    )}"
-}
-
 resource "azurerm_application_insights" "appinsights" {
   name                = "${var.product}-${var.env}"
   location            = var.appinsights_location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = var.application_type
-  tags                = local.ai_tags
+  tags                = var.common_tags
 
   lifecycle {
     ignore_changes = [
@@ -26,4 +16,5 @@ resource "azurerm_application_insights" "appinsights" {
 
 output "appInsightsInstrumentationKey" {
   value = azurerm_application_insights.appinsights.instrumentation_key
+  sensitive = true
 }
--- main.tf
+++ main.tf
@@ -2,18 +2,9 @@ provider "azurerm" {
   features {}
 }
 
-locals {
-  tags = "${merge(
-    var.common_tags,
-    map("Deployment Environment", var.env),
-    map("Team Name", var.team_name),
-    map("Destroy Me", var.destroy_me)
-  )}"
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = var.location
 
-  tags = local.tags
+  tags = var.common_tags
 }
