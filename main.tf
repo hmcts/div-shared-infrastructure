13cb88d545aa6c68ebabe0f9345a9add66de8d3f
Mike Lemos <gobii@me.com>
2023-06-22 13:08:35 +0100

    DFR-1999 shared redis for front end

--- main.tf
+++ main.tf
@@ -8,3 +8,15 @@ resource "azurerm_resource_group" "rg" {
 
   tags = var.common_tags
 }
+
+module "redis-cache" {
+  source      = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product     = "${var.product}-redis"
+  location    = var.location
+  env         = var.env
+  private_endpoint_enabled = true
+  redis_version = "6"
+  business_area = "cft"
+  public_network_access_enabled = false
+  common_tags = var.common_tags
+}
