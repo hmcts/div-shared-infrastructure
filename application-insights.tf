970f6a50e31e0d87f4162bc0ef913c150739632e
manohar.nunna <manohar.nunna@hmcts.net>
2026-04-27 14:39:29 +0100

    DTSPO-30214: Update application-insights module to ref=4.x for azurerm 4.x compatibility

--- application-insights.tf
+++ application-insights.tf
@@ -1,6 +1,6 @@
 
 module "application_insights" {
-  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
 
   env                 = var.env
   product             = var.product
