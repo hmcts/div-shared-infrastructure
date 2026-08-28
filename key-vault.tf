e59e0ed1b475a8de08aef3827f5fe61c85400765
58060 <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2026-08-28 14:42:13 +0000

    Updating Terraform Formatting

--- key-vault.tf
+++ key-vault.tf
@@ -4,17 +4,17 @@ data "azurerm_user_assigned_identity" "jenkins" {
 }
 
 module "div-vault" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
-  name                    = "${var.product}-${var.env}"
-  product                 = var.product
-  env                     = var.env
-  tenant_id               = var.tenant_id
-  object_id               = var.jenkins_AAD_objectId
-  jenkins_object_id       = data.azurerm_user_assigned_identity.jenkins.principal_id
-  resource_group_name     = azurerm_resource_group.rg.name
-  product_group_name      = "dcd_divorce"
-  common_tags             = var.common_tags
-  create_managed_identity = true
+  source                       = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
+  name                         = "${var.product}-${var.env}"
+  product                      = var.product
+  env                          = var.env
+  tenant_id                    = var.tenant_id
+  object_id                    = var.jenkins_AAD_objectId
+  jenkins_object_id            = data.azurerm_user_assigned_identity.jenkins.principal_id
+  resource_group_name          = azurerm_resource_group.rg.name
+  product_group_name           = "dcd_divorce"
+  common_tags                  = var.common_tags
+  create_managed_identity      = true
   grant_preview_jenkins_access = var.env == "aat"
 }
 
